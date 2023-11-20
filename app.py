368124cd863d9dbc74dae8082855248c7bfa78f7
AmrithaP <52408359+AmrithaP@users.noreply.github.com>
2023-11-20 17:06:19 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
+import os
 import pandas as pd
 import streamlit as st
+import dotenv
 from dotenv import load_dotenv
 import matplotlib.pyplot as plt
-import os
+
 #import -r requirements.txt
 
 #from utils.b2 import B2
